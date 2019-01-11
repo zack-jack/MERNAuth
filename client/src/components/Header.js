3a663e832e74c45e7cbefe99e7317c30d9be46b7
Zack Jackson <z.jackson58@gmail.com>
2019-01-10 23:16:34 -0500

    Header nav link conditional auth state rendering

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderNavLinks = () => {
+    if (this.props.authenticated) {
+      return (
+        <div>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/user/logout">Logout</Link>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Link to="/user/register">Register</Link>
+          <Link to="/user/login">Login</Link>
+        </div>
+      );
+    }
+  };
+
   render() {
     return (
       <div>
         <Link to="/">Home</Link>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/user/logout">Logout</Link>
+        {this.renderNavLinks()}
       </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = state => {
+  return {
+    authenticated: state.auth.authenticated
+  };
+};
+
+export default connect(mapStateToProps)(Header);
